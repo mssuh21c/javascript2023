2b79a6784b29930582b978561531d8d7490f508c
Manseok Mark Suh <mssuh21c@gmail.com>
2023-09-27 12:14:29 +0900

    처음 올림
    자바스크립트 과목..  연산자 관련 실습

---
+++ sep27am.js
@@ -0,0 +1,21 @@
+
+
+let person1_name = "홍길동" ;
+let person2_name = "이몽룡" ;
+let person3_name = "성춘향" ;
+
+let person1_age = 19;let person2_age = 30;let person3_age = 25;
+
+let person1_gender = "f";
+let person2_gender = "m";
+let person3_gender = "f";
+
+let person1_residence = 2;
+let person2_residence = 3;
+let person3_residence = 5;
+
+let sum_age =  ( person1_age + person2_age + person3_age )
+let avg_age   = sum_age   /   3
+
+
+console.log ( avg_age)
